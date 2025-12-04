0a4970d280a61cfa05a5408801aede50baa856f9
Paula <124225353+Paula-zp@users.noreply.github.com>
2025-12-04 11:30:30 -0300

    feat: adiciona a função para gerar as chaves

--- grupo-op-comp-quantica/key_generation.py
+++ grupo-op-comp-quantica/key_generation.py
@@ -1,5 +1,5 @@
 import random
-from math_utils import eh_primo
+from math_utils import mdc, inverso_multiplicativo, eh_primo
 
 def gerar_primo(bits=8):
     minimo = 2 ** (bits - 1)
@@ -9,3 +9,29 @@ def gerar_primo(bits=8):
         candidato = random.randint(minimo, maximo)
         if eh_primo(candidato):
             return candidato
+
+def gerar_chaves(tamanho_bits=8):
+
+    p = gerar_primo(tamanho_bits)
+    q = gerar_primo(tamanho_bits)
+    
+    while p == q:
+        q = gerar_primo(tamanho_bits)
+    
+    n = p * q
+    
+    phi_n = (p - 1) * (q - 1)
+    
+    e = 65537
+    
+    if e >= phi_n or mdc(e, phi_n) != 1:
+        e = random.randrange(3, phi_n, 2)
+        while mdc(e, phi_n) != 1:
+            e = random.randrange(3, phi_n, 2)
+    
+    d = inverso_multiplicativo(e, phi_n)
+    
+    chave_publica = (e, n)
+    chave_privada = (d, n)
+    
+    return (chave_publica, chave_privada)
